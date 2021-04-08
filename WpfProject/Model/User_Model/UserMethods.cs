ad6c84b04dbb44efeb57625b36ccb4f912e4f3e6
jktulord <olden112002@mail.ru>
2021-04-08 18:45:41 +0300

    ProfilePageVM update

---
+++ WpfProject/Model/User_Model/UserMethods.cs
@@ -0,0 +1,42 @@
+﻿using Console_prototype.Utils;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+
+namespace WpfProject.Model.User_Model
+{
+    class UserMethods
+    {
+        public static void Save(ObservableCollection<User> list)
+        {
+            ObservableCollection<ConvertedUser> converted_list = new ObservableCollection<ConvertedUser>();
+            foreach (User cur in list)
+            {
+                converted_list.Add(cur.converted);
+            }
+            String path = Directory.GetCurrentDirectory();
+            path = path.Substring(0, path.IndexOf("bin\\Debug")) + "/Savefiles/TariffData.bin";
+            BinarySerialization.WriteToBinaryFile<ObservableCollection<ConvertedUser>>(path, converted_list);
+
+        }
+        public static ObservableCollection<User> Load()
+        {
+            ObservableCollection<User> list = new ObservableCollection<User>();
+            String path = Directory.GetCurrentDirectory();
+            path = path.Substring(0, path.IndexOf("bin\\Debug")) + "/Savefiles/TariffData.bin";
+            ObservableCollection<ConvertedUser> converted_list = BinarySerialization.ReadFromBinaryFile<ObservableCollection<ConvertedUser>>(path);
+
+            int i = 0;
+            foreach (ConvertedUser cur in converted_list)
+            {
+                i++;
+
+                list.Add(new User(cur));
+            }
+            Console.WriteLine("Loaded");
+            return list;
+        }
+    }
+}
---
+++ WpfProject/ViewModel/Pages/ProfilePageVM.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfProject.ViewModel.Pages
+{
+    class ProfilePageVM : ViewModelBase
+    {
+
+
+        public ProfilePageVM()
+        {
+
+        }
+    }
+}
